e3ef793aa6bb7021270961bd30e179b3ec32171a
彭月祥 <1653365777@qq.com>
2021-12-23 09:18:29 +0800

    清除修改不需要的模块

--- src/router/index.js
+++ src/router/index.js
@@ -12,30 +12,6 @@ import nestRouter from './modules/nest';
 //基础路由
 export const baseRoute = [
   {
-    name: 'login',
-    path: '/login',
-    component: () => import('@/views/login/index'),
-    hidden: true
-  },
-  {
-    path: '/403',
-    component: () => import('@/views/error/403'),
-    hidden: true
-  },
-  {
-    path: '/404',
-    component: () => import('@/views/error/404'),
-    hidden: true
-  },
-  {
-    path: '/500',
-    component: () => import('@/views/error/500'),
-    hidden: true
-  }
-];
-
-export const asyncRoutes = [
-  {
     path: '/',
     component: Layout,
     redirect: '/index',
@@ -47,7 +23,7 @@ export const asyncRoutes = [
         component: () => import('@/views/index/index'),
         meta: {
           role: ['admin', 'test'],
-          title: '首页',
+          title: '当日作品',
           icon: 'dashboard'
         }
       },
@@ -57,7 +33,7 @@ export const asyncRoutes = [
         component: () => import('@/views/icon/index'),
         meta: {
           role: ['admin', 'test'],
-          title: '图标',
+          title: '已点评作品库',
           icon: 'icon'
         }
       },
@@ -66,7 +42,7 @@ export const asyncRoutes = [
         path: '/permission',
         component: () => import('@/views/permission/index'),
         meta: {
-          title: '权限测试页',
+          title: '人员作品库',
           icon: 'permission'
         }
       },
@@ -77,7 +53,7 @@ export const asyncRoutes = [
         redirect: '/webGl/ArcGis',
         meta: {
           role: ['admin', 'test'],
-          title: '地图',
+          title: '样片主题库管理',
           icon: 'webGl'
         },
         children: [
@@ -85,64 +61,7 @@ export const asyncRoutes = [
             name: 'ArcGis',
             path: '/webGl/ArcGis',
             component: () => import('@/views/webGl/ArcGis/index'),
-            meta: { title: 'ArcGis'}
-          },
-          {
-            name: 'OpenLayers',
-            path: '/webGl/OpenLayers',
-            component: () => import('@/views/webGl/OpenLayers/index'),
-            meta: { title: '插值分析图' }
-          }
-        ]
-      },
-      echartRouter,
-      componentsRouter,
-      nestRouter,
-      {
-        name: 'error',
-        path: '/error',
-        component: mainLayout,
-        redirect: '/error/403',
-        meta: { title: '错误页面', icon: 'error' },
-        children: [
-          {
-            name: '403',
-            path: '/error/403',
-            component: () => import('@/views/error/403'),
-            meta: { title: '403' }
-          },
-          {
-            name: '404',
-            path: '/error/404',
-            component: () => import('@/views/error/404'),
-            meta: { title: '404' }
-          },
-          {
-            name: '500',
-            path: '/error/500',
-            component: () => import('@/views/error/500'),
-            meta: { title: '500' }
-          }
-        ]
-      },
-      {
-        name: 'userSystem',
-        component: mainLayout,
-        path: '/userSystem',
-        redirect: '/userSystem/userInfo',
-        meta: { title: '个人设置', icon: 'user' },
-        children: [
-          {
-            name: 'userInfo',
-            path: '/userSystem/userInfo',
-            component: () => import('@/views/userSystem/userInfo/index'),
-            meta: { title: '个人中心' }
-          },
-          {
-            name: 'setting',
-            path: '/userSystem/setting',
-            component: () => import('@/views/userSystem/setting/index'),
-            meta: { title: '用户设置' }
+            meta: { title: '优势风格排行'}
           }
         ]
       },
@@ -150,31 +69,19 @@ export const asyncRoutes = [
         name: 'system',
         component: mainLayout,
         path: '/system',
-        redirect: '/system/userManage',
         meta: {
           role: ['admin'],
           title: '系统设置',
           icon: 'system'
-        },
-        children: [
-          {
-            name: 'userManage',
-            path: '/system/userManage',
-            component: () => import('@/views/system/userManage/index'),
-            meta: { title: '用户管理' }
-          },
-          {
-            name: 'roleManage',
-            path: '/system/roleManage',
-            component: () => import('@/views/system/roleManage/index'),
-            meta: { title: '角色管理' }
-          }
-        ]
+        }
       }
     ]
   },
-  { path: '*', redirect: '/404', hidden: true }
+  
+ 
 ];
+
+export const asyncRoutes = [ ];
 
 const createRouter = function() {
   return new VueRouter({
